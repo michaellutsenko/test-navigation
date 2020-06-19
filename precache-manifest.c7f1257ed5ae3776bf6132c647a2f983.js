6b0d03e5676c29dab02316f398a71f3ec90711ea
Michael Lutsenko <Itheadon@users.noreply.github.com>
2020-06-19 20:34:54 +0300

    Updates

---
+++ precache-manifest.c7f1257ed5ae3776bf6132c647a2f983.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d812b61b2ce20b6b111db43502e9f786",
+    "url": "/test-navigation/index.html"
+  },
+  {
+    "revision": "3ddaae374ad675743099",
+    "url": "/test-navigation/static/css/main.640b7063.chunk.css"
+  },
+  {
+    "revision": "a95f49bef78f70c4091a",
+    "url": "/test-navigation/static/js/2.b5a1bef6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/test-navigation/static/js/2.b5a1bef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ddaae374ad675743099",
+    "url": "/test-navigation/static/js/main.9aede6b3.chunk.js"
+  },
+  {
+    "revision": "16a58ce93ff96d811991",
+    "url": "/test-navigation/static/js/runtime-main.9306ce91.js"
+  }
+]);
